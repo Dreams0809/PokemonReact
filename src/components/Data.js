92cacd55a3b6c6035bfd29c1d939c4063b5ae662
Dreams0809 <104542625+Dreams0809@users.noreply.github.com>
2024-04-25 12:42:13 -0400

    Update Data.js

--- src/components/Data.js
+++ src/components/Data.js
@@ -7,23 +7,32 @@ export default {
             attack: 99,
             defense: 54,
             type: "fire",
-            description: "HAHA" 
+            description: "Charmander is a Fire type Pokémon introduced in Generation 1.
+            Charmander is a bipedal, reptilian Pokémon. Most of its body is colored orange, 
+            while its underbelly is light yellow and it has blue eyes. It has a flame at the end of its tail, which is said to signify its health.
+            Charmander's design is based on a lizard, however as its name suggests it may also have been inspired by salamanders, which in mythology have an affinity with fire." 
 
         },
         {
-        pokemon: "Treecko",
-            coverImg: "treecko.png",
+        pokemon: "Bulbasour",
+            coverImg: "bulbasaour.png",
             attack: 99,
             defense: 54,
             type: "grass", 
-            description: "hehe"
+            description: "Bulbasaur is a Grass/Poison type Pokémon introduced in Generation 1. Bulbasaur is a sm mainly turquoise amphibian Pokémon with red eyes and a green bulb on its back. It is based on a frog/toad, 
+            with the bulb resembling a plant bulb that grows into a flower as it evolves.Bulbasaur is notable for being the very first Pokémon in the National Pokédex. 
+            It is one of the three choices for a starter Pokémon in the original Game Boy games, Pokémon Red & Blue (Red & Green in Japan), along with Charmander and Squirtle."
         },
         {
-            pokemon: "Piplup",
-            coverImg: "piplup.png",
+            pokemon: "Squirtle",
+            coverImg: "Squirtle.png",
             attack: 99,
             defense: 54,
-            type: "water", 
+            type: "water",
+            description: "Squirtle is a Water type Pokémon introduced in Generation 1. Squirtle is a bipedal, reptilian Pokémon. 
+            It has a blue body with purple eyes, a light brown belly, and a tough red-brown shell on its back. It has a long tail that curls into a spiral.
+            Squirtle is primarily based on a turtle, taking inspiration from both land-dwelling and sea-dwelling varieties. The curly tail resembles a crashing wave.
+            Squirtle is notable for being one of the three choices for a starter Pokémon in the original Game Boy games, Pokémon Red & Blue (Red & Green in Japan), along with Bulbasaur and Charmander."
         },   
         {
             pokemon: "Genagar",
@@ -31,13 +40,16 @@ export default {
             attack: 99,
             defense: 54,
             type: "ghost", 
+            description: "Gengar is a Ghost/Poison type Pokémon introduced in Generation 1.
+            Gengar has a Mega Evolution, available in Generation 6 games and Pokémon Go; it also has a Gigantamax form available in Pokémon Sword/Shield, with an exclusive G-Max move, G-Max Terror."
         },
         {
-            pokemon: "Charizard",
-            coverImg: "gengar.png",
+            pokemon: "Treecko",
+            coverImg: "treecko.png",
             attack: 99,
             defense: 54,
-            type: "ghost", 
+            type: "ghost",
+            
         },
     ]
     }
